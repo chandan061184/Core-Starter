105a405eaf570387c7a2f6bceab9a13611b04093
Chandan Kumar <chandan061184@outlook.com>
2019-12-01 00:13:20 +0530

    Allowed MVC, added some middleware in request pipeline, setup a route

--- myPieStore/myPieStore/Startup.cs
+++ myPieStore/myPieStore/Startup.cs
@@ -16,6 +16,10 @@ namespace myPieStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Adding MVC support
+            services.AddControllersWithViews();
+            //or
+            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +30,16 @@ namespace myPieStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Welcome to myPieStore!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    ); 
             });
         }
     }
