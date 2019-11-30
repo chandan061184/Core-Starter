b89ba71189e7b41bf64a4e6c58217914505f5fe0
Chandan Kumar <chandan061184@outlook.com>
2019-12-01 00:37:45 +0530

    Added domain(Model class) for Pie and Category

---
+++ myPieStore/myPieStore/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace myPieStore.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public int CategoryName { get; set; }
+        public int Description { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
