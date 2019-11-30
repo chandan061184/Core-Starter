97c4ba993c42cbe562db41a9e6229764d6066332
Chandan Kumar <chandan061184@outlook.com>
2019-12-01 00:46:19 +0530

    Added interface for pie and category repositories

---
+++ myPieStore/myPieStore/Models/IPieRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myPieStore.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPie { get; }
+        IEnumerable<Pie> PieOfTheWeek { get; }
+        Pie GetPieById(int pieId);
+
+    }
+}
